a67b48da795277e41ef2adb19220f26e763d77c9
Medovnik M <medovnikmv@gmail.com>
2026-07-07 14:53:54 +0300

    prisma installed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import authRouter from './modules/auth/auth.route.js';
+import morgan from 'morgan';
+import { env } from '@config/env.js';
 
 const app = express();
+
+if (env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use('/api', authRouter);
